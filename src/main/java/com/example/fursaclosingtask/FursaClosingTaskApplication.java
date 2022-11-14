0de3aea582aaec314cb8d178613f7ad2e728c03c
ahmadabed1 <ahmad.abed2000@gmail.com>
2022-11-14 12:23:35 +0200

    fist commit

---
+++ src/main/java/com/example/fursaclosingtask/FursaClosingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.fursaclosingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FursaClosingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FursaClosingTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fursaclosingtask/FursaClosingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fursaclosingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FursaClosingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
